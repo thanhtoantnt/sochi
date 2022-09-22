a4c998f4573a347445878dd9955097cc53f194a2
thanhtoantnt <thanhtoantnt@gmail.com>
2022-09-22 18:29:19 +0800

    list all solidity files in the input directory

--- src/tools/slither.rs
+++ src/tools/slither.rs
@@ -1,6 +1,6 @@
 //! Module to run Slither
 
-use std::{fs, path::Path};
+use std::{ffi::OsStr, fs, path::Path};
 
 /// Run slither using options
 pub fn run(dir: &str) {
@@ -8,6 +8,11 @@ pub fn run(dir: &str) {
     let path = Path::new(&dir);
     let files = fs::read_dir(path).unwrap();
     for file in files {
-        println!("filename: {}", file.unwrap().path().display());
+        let file = file.unwrap().path();
+        let extension = file.extension().and_then(OsStr::to_str);
+
+        if extension.unwrap() == "sol" {
+            println!("filename: {}", file.display());
+        }
     }
 }
