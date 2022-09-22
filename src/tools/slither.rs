61bad4cff20a54f1b9e19cfa44342d240497c255
thanhtoantnt <thanhtoantnt@gmail.com>
2022-09-22 18:20:16 +0800

    update slither

--- src/tools/slither.rs
+++ src/tools/slither.rs
@@ -1,6 +1,13 @@
 //! Module to run Slither
 
+use std::{fs, path::Path};
+
 /// Run slither using options
-pub fn run(_dir: &str) {
+pub fn run(dir: &str) {
     // List all files in the repository
+    let path = Path::new(&dir);
+    let files = fs::read_dir(path).unwrap();
+    for file in files {
+        println!("filename: {}", file.unwrap().path().display());
+    }
 }
