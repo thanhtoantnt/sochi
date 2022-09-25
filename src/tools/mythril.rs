122237add3b87c95944992e9ed88350ec8e06931
thanhtoantnt <thanhtoantnt@gmail.com>
2022-09-26 01:08:29 +0800

    complete getting mythril results

--- src/tools/mythril.rs
+++ src/tools/mythril.rs
@@ -57,26 +57,32 @@ fn interpret_results(file: &Path) -> Summary {
     let contents =
         fs::read_to_string(file.to_str().unwrap()).expect("Should have been able to read the file");
 
-    let reentrancy_regex = Regex::new(r"==== External Call To User-Supplied Address ====").unwrap();
+    let reentrancy_regex = Regex::new(r"External Call To User-Supplied Address").unwrap();
     let timestamp_dep_regex =
-        Regex::new(r"==== Dependence on predictable environment variable ====").unwrap();
-    let unhandled_exceptions_regex =
-        Regex::new(r"==== Unchecked return value from external call. ====").unwrap();
-    let tx_origin_regex = Regex::new(r"==== Dependence on tx.origin ====").unwrap();
+        Regex::new(r"Dependence on predictable environment variable").unwrap();
+    let unhandled_exceptions_regex = Regex::new(r"Exception State").unwrap();
+    let tx_origin_regex = Regex::new(r"Dependence on tx.origin").unwrap();
+    let integer_regex = Regex::new(r"Integer Arithmetic Bugs").unwrap();
+    let unchecked_send_regex = Regex::new(r"Unchecked return value from external call").unwrap();
+
     let reentrancy = reentrancy_regex.captures_iter(contents.as_str()).count();
     let timestamp_dep = timestamp_dep_regex.captures_iter(contents.as_str()).count();
     let unhandled_exceptions = unhandled_exceptions_regex
         .captures_iter(contents.as_str())
         .count();
     let tx_origin = tx_origin_regex.captures_iter(contents.as_str()).count();
+    let integer_bugs = integer_regex.captures_iter(contents.as_str()).count();
+    let unchecked_send = unchecked_send_regex
+        .captures_iter(contents.as_str())
+        .count();
 
     Summary::new(
         reentrancy,
         timestamp_dep,
-        0,
+        unchecked_send,
         unhandled_exceptions,
         0,
-        0,
+        integer_bugs,
         tx_origin,
     )
 }
@@ -91,6 +97,9 @@ pub fn run_directory(dir: &str) -> Summary {
     let mut timestamp = 0;
     let mut tx_origin = 0;
     let mut unhanled_exceptions = 0;
+    let mut unchecked_send = 0;
+    let mut integer_bugs = 0;
+
     for file in files {
         let file = file.unwrap().path();
         let extension = file.extension().and_then(OsStr::to_str);
@@ -107,6 +116,8 @@ pub fn run_directory(dir: &str) -> Summary {
                     timestamp += result.timestamp;
                     unhanled_exceptions += result.unhandled_exceptions;
                     tx_origin += result.tx_origin;
+                    integer_bugs += result.integer_flow;
+                    unchecked_send += result.unchecked_send;
                     debug!("bugs: {}", result);
                 }
                 Err(msg) => {
@@ -119,10 +130,10 @@ pub fn run_directory(dir: &str) -> Summary {
     Summary::new(
         reentrancy,
         timestamp,
-        0,
+        unchecked_send,
         unhanled_exceptions,
         0,
-        0,
+        integer_bugs,
         tx_origin,
     )
 }
