ae8f740eb9ecb841c3a346bea8c952165dd86ff4
thanhtoantnt <thanhtoantnt@gmail.com>
2022-09-26 07:42:44 +0000

    experiment with adding files from server

---
+++ mythril_script.sh
@@ -0,0 +1,2 @@
+myth analyze --execution-timeout 60 --solv 0.5.16 tests/re-entrancy/buggy_1.sol > tests/re-entrancy/buggy_1.myth
+myth analyze --execution-timeout 60 --solv 0.5.1 tests/re-entrancy/buggy_2.sol > tests/re-entrancy/buggy_2.myth
