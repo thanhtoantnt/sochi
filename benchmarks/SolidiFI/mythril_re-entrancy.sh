9003065426e61a2fa681fa8885a25163d70260cc
thanhtoantnt <thanhtoantnt@gmail.com>
2022-09-27 12:00:41 +0800

    add results

--- benchmarks/SolidiFI/mythril_re-entrancy.sh
+++ benchmarks/SolidiFI/mythril_re-entrancy.sh
@@ -24,38 +24,38 @@ echo "myth analyze --execution-timeout 60 --solv 0.5.1 Re-entrancy/buggy_2.sol >
 myth analyze --execution-timeout 60 --solv 0.5.1 Re-entrancy/buggy_2.sol > Re-entrancy/buggy_2.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_20.sol > Re-entrancy/buggy_20.myth"
 myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_20.sol > Re-entrancy/buggy_20.myth
-echo "myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_21.sol > Re-entrancy/buggy_21.myth"
-myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_21.sol > Re-entrancy/buggy_21.myth
+echo "myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_21.sol > Re-entrancy/buggy_21.myth"
+myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_21.sol > Re-entrancy/buggy_21.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.1 Re-entrancy/buggy_22.sol > Re-entrancy/buggy_22.myth"
 myth analyze --execution-timeout 60 --solv 0.5.1 Re-entrancy/buggy_22.sol > Re-entrancy/buggy_22.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.2 Re-entrancy/buggy_23.sol > Re-entrancy/buggy_23.myth"
 myth analyze --execution-timeout 60 --solv 0.5.2 Re-entrancy/buggy_23.sol > Re-entrancy/buggy_23.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.0 Re-entrancy/buggy_24.sol > Re-entrancy/buggy_24.myth"
 myth analyze --execution-timeout 60 --solv 0.5.0 Re-entrancy/buggy_24.sol > Re-entrancy/buggy_24.myth
-echo "myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_25.sol > Re-entrancy/buggy_25.myth"
-myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_25.sol > Re-entrancy/buggy_25.myth
+echo "myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_25.sol > Re-entrancy/buggy_25.myth"
+myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_25.sol > Re-entrancy/buggy_25.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.2 Re-entrancy/buggy_26.sol > Re-entrancy/buggy_26.myth"
 myth analyze --execution-timeout 60 --solv 0.5.2 Re-entrancy/buggy_26.sol > Re-entrancy/buggy_26.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.0 Re-entrancy/buggy_27.sol > Re-entrancy/buggy_27.myth"
 myth analyze --execution-timeout 60 --solv 0.5.0 Re-entrancy/buggy_27.sol > Re-entrancy/buggy_27.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.0 Re-entrancy/buggy_28.sol > Re-entrancy/buggy_28.myth"
 myth analyze --execution-timeout 60 --solv 0.5.0 Re-entrancy/buggy_28.sol > Re-entrancy/buggy_28.myth
-echo "myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_29.sol > Re-entrancy/buggy_29.myth"
-myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_29.sol > Re-entrancy/buggy_29.myth
+echo "myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_29.sol > Re-entrancy/buggy_29.myth"
+myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_29.sol > Re-entrancy/buggy_29.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.1 Re-entrancy/buggy_3.sol > Re-entrancy/buggy_3.myth"
 myth analyze --execution-timeout 60 --solv 0.5.1 Re-entrancy/buggy_3.sol > Re-entrancy/buggy_3.myth
-echo "myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_30.sol > Re-entrancy/buggy_30.myth"
-myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_30.sol > Re-entrancy/buggy_30.myth
-echo "myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_31.sol > Re-entrancy/buggy_31.myth"
-myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_31.sol > Re-entrancy/buggy_31.myth
+echo "myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_30.sol > Re-entrancy/buggy_30.myth"
+myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_30.sol > Re-entrancy/buggy_30.myth
+echo "myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_31.sol > Re-entrancy/buggy_31.myth"
+myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_31.sol > Re-entrancy/buggy_31.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.7 Re-entrancy/buggy_32.sol > Re-entrancy/buggy_32.myth"
 myth analyze --execution-timeout 60 --solv 0.5.7 Re-entrancy/buggy_32.sol > Re-entrancy/buggy_32.myth
-echo "myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_33.sol > Re-entrancy/buggy_33.myth"
-myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_33.sol > Re-entrancy/buggy_33.myth
-echo "myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_34.sol > Re-entrancy/buggy_34.myth"
-myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_34.sol > Re-entrancy/buggy_34.myth
-echo "myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_35.sol > Re-entrancy/buggy_35.myth"
-myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_35.sol > Re-entrancy/buggy_35.myth
+echo "myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_33.sol > Re-entrancy/buggy_33.myth"
+myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_33.sol > Re-entrancy/buggy_33.myth
+echo "myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_34.sol > Re-entrancy/buggy_34.myth"
+myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_34.sol > Re-entrancy/buggy_34.myth
+echo "myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_35.sol > Re-entrancy/buggy_35.myth"
+myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_35.sol > Re-entrancy/buggy_35.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.1 Re-entrancy/buggy_36.sol > Re-entrancy/buggy_36.myth"
 myth analyze --execution-timeout 60 --solv 0.5.1 Re-entrancy/buggy_36.sol > Re-entrancy/buggy_36.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.0 Re-entrancy/buggy_37.sol > Re-entrancy/buggy_37.myth"
@@ -70,26 +70,26 @@ echo "myth analyze --execution-timeout 60 --solv 0.5.0 Re-entrancy/buggy_40.sol 
 myth analyze --execution-timeout 60 --solv 0.5.0 Re-entrancy/buggy_40.sol > Re-entrancy/buggy_40.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.16 Re-entrancy/buggy_41.sol > Re-entrancy/buggy_41.myth"
 myth analyze --execution-timeout 60 --solv 0.5.16 Re-entrancy/buggy_41.sol > Re-entrancy/buggy_41.myth
-echo "myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_42.sol > Re-entrancy/buggy_42.myth"
-myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_42.sol > Re-entrancy/buggy_42.myth
+echo "myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_42.sol > Re-entrancy/buggy_42.myth"
+myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_42.sol > Re-entrancy/buggy_42.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.0 Re-entrancy/buggy_43.sol > Re-entrancy/buggy_43.myth"
 myth analyze --execution-timeout 60 --solv 0.5.0 Re-entrancy/buggy_43.sol > Re-entrancy/buggy_43.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.0 Re-entrancy/buggy_44.sol > Re-entrancy/buggy_44.myth"
 myth analyze --execution-timeout 60 --solv 0.5.0 Re-entrancy/buggy_44.sol > Re-entrancy/buggy_44.myth
-echo "myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_45.sol > Re-entrancy/buggy_45.myth"
-myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_45.sol > Re-entrancy/buggy_45.myth
+echo "myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_45.sol > Re-entrancy/buggy_45.myth"
+myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_45.sol > Re-entrancy/buggy_45.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.16 Re-entrancy/buggy_46.sol > Re-entrancy/buggy_46.myth"
 myth analyze --execution-timeout 60 --solv 0.5.16 Re-entrancy/buggy_46.sol > Re-entrancy/buggy_46.myth
-echo "myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_47.sol > Re-entrancy/buggy_47.myth"
-myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_47.sol > Re-entrancy/buggy_47.myth
-echo "myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_48.sol > Re-entrancy/buggy_48.myth"
-myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_48.sol > Re-entrancy/buggy_48.myth
-echo "myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_49.sol > Re-entrancy/buggy_49.myth"
-myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_49.sol > Re-entrancy/buggy_49.myth
+echo "myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_47.sol > Re-entrancy/buggy_47.myth"
+myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_47.sol > Re-entrancy/buggy_47.myth
+echo "myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_48.sol > Re-entrancy/buggy_48.myth"
+myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_48.sol > Re-entrancy/buggy_48.myth
+echo "myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_49.sol > Re-entrancy/buggy_49.myth"
+myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_49.sol > Re-entrancy/buggy_49.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.16 Re-entrancy/buggy_5.sol > Re-entrancy/buggy_5.myth"
 myth analyze --execution-timeout 60 --solv 0.5.16 Re-entrancy/buggy_5.sol > Re-entrancy/buggy_5.myth
-echo "myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_50.sol > Re-entrancy/buggy_50.myth"
-myth analyze --execution-timeout 60 --solv 0.5.11 Re-entrancy/buggy_50.sol > Re-entrancy/buggy_50.myth
+echo "myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_50.sol > Re-entrancy/buggy_50.myth"
+myth analyze --execution-timeout 60 --solv 0.5.10 Re-entrancy/buggy_50.sol > Re-entrancy/buggy_50.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.8 Re-entrancy/buggy_6.sol > Re-entrancy/buggy_6.myth"
 myth analyze --execution-timeout 60 --solv 0.5.8 Re-entrancy/buggy_6.sol > Re-entrancy/buggy_6.myth
 echo "myth analyze --execution-timeout 60 --solv 0.5.8 Re-entrancy/buggy_7.sol > Re-entrancy/buggy_7.myth"
