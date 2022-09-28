f570a6533049c97240e6eeb1b514242ac6638bb8
thanhtoantnt <thanhtoantnt@gmail.com>
2022-09-28 16:43:16 +0800

    add script to run confuzzius

---
+++ confuzzius.sh
@@ -0,0 +1,6 @@
+./sochi --tool confuzzius --gen-results benchmarks/SolidiFI/Unhandled-Exceptions/
+./sochi --tool confuzzius --gen-results benchmarks/SolidiFI/TOD/
+./sochi --tool confuzzius --gen-results benchmarks/SolidiFI/Overflow-Underflow/
+./sochi --tool confuzzius --gen-results benchmarks/SolidiFI/tx.origin/
+
+
