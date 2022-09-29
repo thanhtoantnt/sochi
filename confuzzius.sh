46bf30edaf34f9f42c4c7db6cd7c059c9aad8c93
thanhtoantnt <thanhtoantnt@gmail.com>
2022-09-29 10:43:06 +0800

    add new confuzzius commands

--- confuzzius.sh
+++ confuzzius.sh
@@ -1,7 +1,18 @@
-./sochi --tool confuzzius --gen-results benchmarks/SolidiFI/Unhandled-Exceptions/
-./sochi --tool confuzzius --gen-results benchmarks/SolidiFI/TOD/
-./sochi --tool confuzzius --gen-results benchmarks/SolidiFI/Overflow-Underflow/
-./sochi --tool confuzzius --gen-results benchmarks/SolidiFI/tx.origin/
-./sochi --tool confuzzius --gen-results benchmarks/SolidiFI/Unchecked-Send/
+# ./sochi --tool confuzzius --gen-results benchmarks/SolidiFI/Unhandled-Exceptions/
+# ./sochi --tool confuzzius --gen-results benchmarks/SolidiFI/TOD/
+# ./sochi --tool confuzzius --gen-results benchmarks/SolidiFI/Overflow-Underflow/
+# ./sochi --tool confuzzius --gen-results benchmarks/SolidiFI/tx.origin/
+# ./sochi --tool confuzzius --gen-results benchmarks/SolidiFI/Unchecked-Send/
+
+./sochi --tool confuzzius --gen-results benchmarks/Confuzzius/assertion_failure/
+./sochi --tool confuzzius --gen-results benchmarks/Confuzzius/block_dependency/
+./sochi --tool confuzzius --gen-results benchmarks/Confuzzius/integer_overflows/
+./sochi --tool confuzzius --gen-results benchmarks/Confuzzius/leaking_ether/
+./sochi --tool confuzzius --gen-results benchmarks/Confuzzius/locking_ether/
+./sochi --tool confuzzius --gen-results benchmarks/Confuzzius/transaction_order_dependency/
+./sochi --tool confuzzius --gen-results benchmarks/Confuzzius/unhandled_exception/
+./sochi --tool confuzzius --gen-results benchmarks/Confuzzius/unsafe_delegatecall/
+
+
 
 
