772d22f4c0ffc3e8f392679b01cbd4adfad5a936
thanhtoantnt <thanhtoantnt@gmail.com>
2022-11-03 17:56:34 +0800

    re-do experiments for Slither re-entrancy

--- src/tools/slither.rs
+++ src/tools/slither.rs
@@ -5,7 +5,7 @@ use super::Summary;
 use regex::Regex;
 use std::io::prelude::*;
 use std::path::PathBuf;
-use std::{ffi::OsStr, fs, fs::File, path::Path, process::Command};
+use std::{ffi::OsStr, fs, fs::File, io::BufReader, path::Path, process::Command};
 
 /// Run slither for each file
 fn run_slither(input_file_path: PathBuf) -> Result<PathBuf, String> {
@@ -89,17 +89,63 @@ fn check_slither_results(file: &Path) -> i32 {
     let parent_dir = file.parent().unwrap_or_else(|| Path::new(""));
     let result_file = parent_dir.join(file_stem_name.to_owned() + "." + super::SLITHER);
     debug!("Reading file: {}", result_file.display());
-    let contents = fs::read_to_string(result_file.to_str().unwrap())
-        .expect("Should have been able to read the file");
+    let file = File::open(result_file.to_str().unwrap().to_string());
+    let reader = BufReader::new(file.unwrap());
+    let slither_regex = Regex::new(r"sol\#(\d+)-").unwrap();
+    let mut positions = vec![];
 
-    let slither_regex = Regex::new(r"sol\#(\d+)\)").unwrap();
-
-    for found in slither_regex.captures_iter(&contents) {
-        let foundx = found.get(1).map_or("", |c| c.as_str());
-        println!("found: {:?}", foundx);
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if line.contains("Reentrancy in") {
+            for found in slither_regex.captures_iter(&line) {
+                let foundx = found.get(1).map_or("", |c| c.as_str());
+                debug!("found: {:?}", foundx);
+                let number = foundx.parse::<i32>().unwrap();
+                positions.push(number);
+            }
+        }
     }
 
-    0
+    let baseline_file = parent_dir.join(file_stem_name.to_owned() + "." + super::CSV);
+    let baseline = File::open(baseline_file.to_str().unwrap().to_string());
+    let baseline_reader = BufReader::new(baseline.unwrap());
+    let mut baseline_locations = vec![];
+
+    for line in baseline_reader.lines() {
+        //
+        let line = line.unwrap();
+        let elements: Vec<&str> = line.split(',').collect();
+        let location = elements[0];
+        let range = elements[1];
+        let number_opt = location.parse::<i32>();
+        if let Ok(number) = number_opt {
+            let range = range.parse::<i32>().unwrap();
+            baseline_locations.push((number, range));
+        }
+    }
+
+    let mut counter = 0;
+    for position in positions {
+        for (number, range) in baseline_locations.clone() {
+            if position >= number && position <= number + range {
+                counter += 1;
+            }
+        }
+    }
+
+    // for positions
+
+    // let contents = fs::read_to_string(result_file.to_str().unwrap())
+    //     .expect("Should have been able to read the file");
+
+    // let slither_regex = Regex::new(r"sol\#(\d+)\)").unwrap();
+
+    // for found in slither_regex.captures_iter(&contents) {
+    //     let foundx = found.get(1).map_or("", |c| c.as_str());
+    //     println!("found: {:?}", foundx);
+    // }
+
+    counter
 }
 
 /// Check slither results to be false/true positives
@@ -122,6 +168,8 @@ pub fn check_results(dir: &str) -> i32 {
             debug!("bugs: {}", result);
         }
     }
+
+    println!("Total: {}", summary);
 
     summary
 }
